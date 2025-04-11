b937b70accc700f4a4d1a78e3c468e5101fd1c45
Riyaz Uddin <riyazps007@gmail.com>
2025-04-11 12:02:33 +0530

    Code refactor

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-let express = require('express');
-let app = express();
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 
-app.get("/api/:date", (req, res) => {
-	const toTimestamp = date => Math.floor(date.getTime() / 1000);
-	const fromTimestamp = timestamp => new Date(timestamp * 1000);
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
 
-	let unixTimestamp;
-	let date;
-	const dateString = req.params.date;
-	if(dateString.includes('-')){
-		date = new Date(dateString);
-		unixTimestamp = toTimestamp(date);
 
-	} else{
-		date = fromTimestamp(dateString);
-		unixTimestamp = toTimestamp(date);
-	}
-	res.json({ unix: unixTimestamp, utc: date.toUTCString() })
-})
 
-app.listen(80, () => {
-	console.log('intercepter');
-})
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
