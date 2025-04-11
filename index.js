f9da16ca8cfbb77b8b1c9f2e470ae5f5915ffc45
Riyaz Uddin <riyazps007@gmail.com>
2025-04-11 11:45:50 +0530

    Fixed /api/:date

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ let app = express();
 
 
 app.get("/api/:date", (req, res) => {
-	const dateString = req.params.date;
-	const date = Date(dateString).toString();
-	res.json({ unix: dateString, utc: date })
+	const toTimestamp = date => Math.floor(date.getTime() / 1000);
+	const fromTimestamp = timestamp => new Date(timestamp * 1000);
 
+	let unixTimestamp;
+	let date;
+	const dateString = req.params.date;
+	if(dateString.includes('-')){
+		date = new Date(dateString);
+		unixTimestamp = toTimestamp(date);
+
+	} else{
+		date = fromTimestamp(dateString);
+		unixTimestamp = toTimestamp(date);
+	}
+	res.json({ unix: unixTimestamp, utc: date.toUTCString() })
 })
 
 app.listen(80, () => {
